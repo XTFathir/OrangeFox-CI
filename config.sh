5c4671c159a8321cd0d7e1df713a39b19fa17b38
XTFathir <104472022+XTFathir@users.noreply.github.com>
2023-01-23 12:26:12 +0700

    Update config.sh

--- config.sh
+++ config.sh
@@ -22,7 +22,7 @@ DEPS=(
 )
 
 # Extra Command
-export EXTRA_CMD="export OF_MAINTAINER=FTRQP"
+export EXTRA_CMD="export OF_MAINTAINER=FTRQPO"
 
 # Magisk
 ## Use the Latest Release of Magisk for the OrangeFox addon
